ff3e93d1f2dc2bebe67dd231e67dc34e07527b78
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-12-20 20:48:48 -0600

    Componente IpDetails

--- ip-address-tracker/src/components/Hero.js
+++ ip-address-tracker/src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form } from './Form';
+import { IpDetails } from './IpDetails';
 
 export const Hero = () => {
 
@@ -7,6 +8,7 @@ export const Hero = () => {
         <div className="hero__container">
             <h1>IP Address Tracker</h1>
             <Form />
+            <IpDetails />
         </div>
     );
 
---
+++ ip-address-tracker/src/components/IpDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const IpDetails = () => {
+
+    return (
+        <div className="ipDetails__container">
+            <h1>IpDetails</h1>
+        </div>
+    );
+
+};
